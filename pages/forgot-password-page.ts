40a9b30b1ce9c12e138cdcab3fa732a116999b7d
amakarenko1203 <annamakarenko.rdh@gmail.com>
2025-09-24 21:09:46 -0500

    ran the test

--- pages/forgot-password-page.ts
+++ pages/forgot-password-page.ts
@@ -1,18 +1,18 @@
 import { Locator, expect, Page } from '@playwright/test'
-import { HomePage } from './home-page'
+
 
 export class ForgotPasswordClass {
     titleLocator: Locator
     emailField: Locator
     retreivePassButton: Locator
-    errorMessage: Locator
+   
 
 
     constructor(page: Page) {
         this.titleLocator = page.getByRole('heading', { name: 'Forgot Password' })
         this.emailField = page.getByRole('textbox', { name: 'E-mail' })
         this.retreivePassButton = page.getByRole('button', { name: 'Retrieve password' })
-        this.errorMessage = page.getByRole('heading', { name: 'Internal Server Error' })
+    
     }
     
     async validatePageTitle(): Promise<void> {
@@ -25,9 +25,5 @@ export class ForgotPasswordClass {
     async clickResetButton(): Promise<void> {
         await this.retreivePassButton.click()
     }
-    async validateInternalServerError(): Promise<void> {
-
-        await expect(this.errorMessage).toHaveText('Internal Server Error')
-
-    }
+   
 }
